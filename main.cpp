ada52fc53978b837c757425695c2c0659d7e5b87
NewlAsh <kalashdesai.kd@gmail.com>
2025-12-28 13:32:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,47 +3,57 @@ using namespace std;
 
 int main() {
     
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
-
-    if(!fin ||  !fout){
-        cout<<"ERROR"<<endl;
-    }
+    ifstream fin("in.txt");
+    ofstream fout("out.txt");
     string line;
-    int idx=1;
+    int idx = 1;
 
     while(getline(fin,line)){
-        if(line.size()==0) continue;
-
-
         stringstream ss(line);
         int x;
         
         vector<int>v;
-        map<int,int>mpp;
-        
-        int cnt=0, sum=0;
+        map<int, int>mpp;
+        int cnt=0, sum=0, modeI = INT_MIN, mode=-1;
+
         while(ss >> x){
             v.push_back(x);
             cnt++;
             sum += x;
             mpp[x]++;
         }
-        fout<<"Array "<<idx<<" is: ";
+        bool found = false;
+        for(auto it : mpp){
+            if(it.second>modeI && it.second>1){
+                modeI = it.second;
+                mode = it.first;
+                found=true;
+            }
+        }
+
+        fout<<"Array "<<idx<<" is:   ";
         for(auto it : v){
-            fout<<it<<"  ";
+            fout<<it<<" ";
         }
         fout<<endl<<endl;
-
-        fout<<"No. of elements in Array "<<idx<<" is: "<<cnt<<endl<<endl;
-        fout<<"Sum of those elements is: "<<sum<<endl<<endl;
-        fout<<"Below mentioned are the frequencies of each element: "<<endl;
+        fout<<"no. of elements are:   "<<cnt<<endl<<endl;
+        fout<<"frequencies are mentioned below for each element of the array: "<<endl;
         for(auto it : mpp){
-            fout<<it.first<<" occurs "<<it.second<<" times"<<endl;
+            fout<<it.first<<" repeats "<<it.second<<" times"<<endl;
         }
-        fout<<endl<<endl<<endl;
-
+        fout<<endl<<endl;
+        if(!found){
+            fout<<"No mode for this data-set"<<endl<<endl;
+        }
+        else{
+            fout<<"Mode for this is: "<<mode<<endl<<endl;
+        }
+        fout<<"This makes the sum equal to: "<<sum<<endl<<endl;
+        float mean = float(sum)/cnt;
+        fout<<"Mean for this data-set is: "<<(mean)<<endl<<endl;
         idx++;
+
+        fout<<endl<<endl;
     }
 
     
